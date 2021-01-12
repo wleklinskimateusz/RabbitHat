09d91d9cf14b9edb5ccda601ba4da3424b8aa9ce
Mateusz Wleklinski <matix0508@gmail.com>
2021-01-12 11:15:24 +0100

    API

---
+++ Api/ProjectsFunction.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+using BlazorApp.Client.Models;
+using System.Collections.Generic;
+
+namespace BlazorApp.Api
+{
+    public static class ProjectsFunction
+    {
+        [FunctionName("Projects")]
+        public static IActionResult Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+
+            Project Sudoku = new Project("SudokuSolver", "Unity", new DateTime(2020, 12, 28), "https://simmer.io/@matix0508/sudokusolver");
+            Sudoku.Description = "Short Description";
+            var result = new List<Project>()
+            {
+                Sudoku
+            };
+
+            Console.WriteLine("Hello");
+
+            return new OkObjectResult(result.ToArray());
+        }
+    }
+}
--- Client/Models/Project.cs
+++ Client/Models/Project.cs
@@ -8,16 +8,16 @@ namespace BlazorApp.Client.Models
     public class Project
     {
         public string Name { get; set; }
-        public List<Technology> Technologies { get; set; }
+        public Technology Technology { get; set; }
         public DateTime StartingTime { get; set; }
         public DateTime EndTime { get; set; }
         public string Description { get; set; }
         public string URL { get; set; }
         
-        public Project(string name, List<Technology> techs, DateTime start, DateTime end)
+        public Project(string name, Technology techs, DateTime start, DateTime end)
         {
             Name = name;
-            Technologies = techs;
+            Technology = techs;
             StartingTime = start;
             EndTime = end;
         }
@@ -25,22 +25,20 @@ namespace BlazorApp.Client.Models
         {
 
         }
-        public Project(string name, List<string> techs, DateTime start, DateTime end)
+
+        public Project(string name, string tech, DateTime start)
         {
             Name = name;
-            Technologies = StringToTech(techs);
+            Technology = new Technology(tech);
             StartingTime = start;
-            EndTime = end;
         }
 
-        List<Technology> StringToTech(List<string> lst)
+        public Project(string name, string tech, DateTime start, string url)
         {
-            List<Technology> output = new List<Technology>();
-            foreach(var item in lst)
-            {
-                output.Add(new Technology(item));
-            }
-            return output;
+            Name = name;
+            Technology = new Technology(tech);
+            StartingTime = start;
+            URL = url;
         }
     }
 }
