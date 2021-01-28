5a54efbf1817a1a2fc37c547800bfc768e01ebb8
Mateusz Wleklinski <matix0508@gmail.com>
2021-01-28 19:46:20 +0100

    polls

---
+++ Shared/Polls.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorApp.Shared
+{
+    class Polls
+    {
+        public string Name;
+        public string Topic;
+        public string URL;
+
+        public Polls()
+        {
+
+        }
+
+        public Polls(string name, string topic, string url)
+        {
+            Name = name;
+            Topic = topic;
+            URL = url;
+        }
+    }
+}
